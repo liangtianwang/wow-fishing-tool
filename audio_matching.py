b0b36ab73714990556322b54db30075c6180ff2e
liangtianwang <ltw.work@outlook.com>
2024-06-05 14:36:19 +1000

    update

--- audio_matching.py
+++ audio_matching.py
@@ -2,28 +2,44 @@ import sounddevice as sd
 import numpy as np
 import time
 
-st = sd.Stream()
-
-def set_stream(callback):
- return sd.Stream(callback=callback)
-
-def get_stream():
-   return st
 
 
-def sound_capture(max_wait_second):
-    time.sleep(2)
+class audio_matching():
+    def __init__(self, volume_threshold, monitor):
+        self.volume_threshold = volume_threshold
+        self.monitor = monitor
+        self.st = sd.Stream(callback=self.print_sound)
+
+    def print_sound(self, indata, outdata, frames, time, status):
+        volume_norm = np.linalg.norm(indata)*10
+        volume = int(volume_norm)
+        if self.monitor:
+            print('|' * volume)
+            #print(volume_norm)
+        if(volume > self.volume_threshold):
+            self.fish_action()
+            self.st.stop()
+
+    def get_stream(self):
+        return self.st
     
-    st.start()
-    print('Audio capture process started')
-    
-    i = 0
-    while st.active:
-        sd.sleep(1000)
-        i = i + 1
-        if i > max_wait_second:
-            print('Reached maximum wait second, stopped audio capture and give up fishing')
-            st.stop()
-            break
-    
-    print('Audio capture process stopped')
+    def set_action_when_getting_fish(self, fish_action):
+        self.fish_action = fish_action
+
+    def sound_capture(self, max_wait_second):    
+        time.sleep(2)
+        
+        self.st.start()
+        print('Audio capture process started')
+        #print(sd.default.device)
+        
+        i = 0
+        while self.st.active:
+            sd.sleep(1000)
+            i = i + 1
+            if i > max_wait_second:
+                print('Reached maximum wait second, stopped audio capture and give up fishing')
+                self.st.stop()
+                break
+        
+        print('Audio capture process stopped')
--- main.py
+++ main.py
@@ -13,22 +13,19 @@ sound_path = './soundID'
 coordinate = 0.69  #数值越高越难定位但是越准确
 retry = 25
 volume_threshold = 30
+enable_volume_monitor = False
 
-def print_sound(indata, outdata, frames, time, status):
-    volume_norm = np.linalg.norm(indata)*10
-    volume = int(volume_norm)
-    print('|' * volume)
-    if(volume > volume_threshold):
-        ag.rightClick()
-        am.get_stream().stop()
 
-am.set_stream(print_sound)
+
+ami = am.audio_matching(volume_threshold, enable_volume_monitor)
+ami.set_action_when_getting_fish(lambda: {ag.rightClick()})
+st = ami.get_stream()
 
 while(True):
-    am.get_stream().start()
-    print("Sleep 2 seconds before previous fishing icon disappea111rs")
+    st.start()
+    print("Sleep 2 seconds before previous fishing icon disappears")
     time.sleep(2)
-    am.get_stream().stop()
+    st.stop()
 
     print("Start to capture fishing icon position")
     fish_position = im.locate_image(image_path, fishing_icons, coordinate,retry)
@@ -41,7 +38,10 @@ while(True):
             print('incorrect mouse move')
         else:			
             print("Moved to fishing icon position")
-            am.sound_capture(print_sound)
+            ami.sound_capture(volume_threshold)
 
     print("Cast to fish for next round...")
+    print("------------------------------")
+    print("##########New Round##########")
+    print("------------------------------")
     ag.press('1')
